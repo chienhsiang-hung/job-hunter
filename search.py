6fc18b23502e3cdf6f9596cbde395b49c3732a7e
洪健翔 Hung, Chien-Hsiang <abcdefg2981@gmail.com>
2024-10-16 03:53:43 +0000

    add check_joblists

--- search.py
+++ search.py
@@ -2,6 +2,24 @@ import json, sys
 import requests
 import pandas as pd
 
+def check_joblists(company_id):
+    url = f'https://www.104.com.tw/company/ajax/joblist/options/{company_id}'
+    headers = {
+        'Host': 'www.104.com.tw',
+        'Referer': f'https://www.104.com.tw/company/{company_id}',
+
+        # mimic the headers, payload, user-agent, etc.
+        # https://stackoverflow.com/questions/42237672/python-toomanyredirects-exceeded-30-redirects
+        'Accept-Encoding': 'gzip, deflate, sdch',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Cache-Control': 'max-age=0',
+        'Connection': 'keep-alive',
+    }
+    main_r = requests.get(url, headers=headers)
+    return main_r.json()['data']['jobCount'] != 0
 
 def search(searched_word='power platform', company='Realtek'):
     """
@@ -13,16 +31,20 @@ def search(searched_word='power platform', company='Realtek'):
         company_dict = json.load(my_json)
     company_id = company_dict[company]
 
+    potential_jobs = []
+
+    if not check_joblists(company_id):
+        print(f'There is no jobs opening in {company}')
+        return potential_jobs
+
     url = f'https://www.104.com.tw/company/ajax/joblist/{company_id}'
     headers = {
         'Host': 'www.104.com.tw',
         'Referer': f'https://www.104.com.tw/company/{company_id}'
     }
     main_r = requests.get(url, headers=headers)
-
     pages = main_r.json()['data']['totalPages']
 
-    potential_jobs = []
     # outer loop for pages
     for p in range(1, int(pages)+1):
         sub_r = requests.get(url+f'?page={p}', headers=headers)
