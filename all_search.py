5f438e19a45ecb3a978ac4f0aeb783d900576fbb
洪健翔 Hung, Chien-Hsiang <abcdefg2981@gmail.com>
2024-10-16 06:31:39 +0000

    json.dump()

--- all_search.py
+++ all_search.py
@@ -7,12 +7,16 @@ with open('company-match.json', 'r') as my_json:
 
 search = Search()
 potential_jobs = []
-for company in company_dict:
+for i, company in enumerate(company_dict):
+    # enumerate for testing purpose
+    # if i==2: break
     result = search.search(searched_word='power automate', company=company)
     if len(result) > 0: potential_jobs += result
 
 df = pd.DataFrame.from_records(potential_jobs)
 df.to_excel(f'Result.xlsx')
+with open('Result.json', 'w') as f:
+    json.dump(potential_jobs, f, indent=4)
 
 print(f'Total len={len(potential_jobs)}')
 print(df)
